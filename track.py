e682174d1caa9bd8a2a1b7b10e73d0823c98c277
Naveen <59352108+Naveen-Enock@users.noreply.github.com>
2022-11-05 20:59:13 -0400

    Add files via upload

---
+++ track.py
@@ -0,0 +1,223 @@
+import cv2
+import mediapipe as mp
+import time
+import os
+import math
+
+cap = cv2.VideoCapture(0)
+def detect_a():
+    #70 on pinky when doing A
+    #Thumb must be more than 120
+    if distance(xvals,yvals,"PINKY_TIP") > 88:
+        return False
+    elif distance(xvals,yvals,"RING_TIP") > 98:
+        return False
+    elif distance(xvals,yvals,"MIDDLE_TIP") > 98:
+        return False
+    elif distance(xvals,yvals,"INDEX_TIP") > 98:
+        return False
+    elif distance(xvals,yvals,"THUMB_TIP") < 150:
+        return False
+    else:
+        return True
+def detect_b():
+    #160 100
+    #40 210
+    if distance(xvals,yvals,'THUMB_TIP') > 120:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') < 190:
+        return False
+    elif distance(xvals,yvals,'INDEX_TIP') < 190:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') < 190:
+        return False
+    elif distance(xvals,yvals,'PINKY_TIP') < 150:
+        return False
+    else:
+        return True
+def detect_c():
+    # Thumb must be at least 170 pixels away
+    #d for pinky 250.83859352181037
+    #d for ring 267.6191323504357
+    #d for middle 273.48126078398866
+    #d for index 257.70137756713683
+    #d for thumb 211.24866863485792
+
+    if distance(xvals,yvals,'THUMB_TIP') < 170:
+        return False
+    elif abs(distance(xvals,yvals,'PINKY_TIP') - distance(xvals,yvals,'RING_TIP')) > 20:
+        return False
+    elif abs(distance(xvals,yvals,'RING_TIP') - distance(xvals,yvals,'MIDDLE_TIP')) > 20:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') < 260:
+        return False
+    elif distance(xvals,yvals,'INDEX_TIP') < 240:
+        return False
+    else:
+        return True
+def detect_d():
+    if distance(xvals,yvals,'INDEX_TIP') < 140:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') < 90:
+        return False
+    elif abs(distance(xvals,yvals,'RING_TIP') - distance(xvals,yvals,'MIDDLE_TIP')) > 20:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') > 60:
+        return False
+    elif distance(xvals,yvals,'PINKY_TIP') > 60:
+        return False
+    else:
+        return True
+def detect_e():
+    # Thumb should be 55
+    if distance(xvals,yvals,'THUMB_TIP') > 65:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') < 45:
+        return False
+    elif distance(xvals,yvals,'PINKY_TIP') > 98:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') > 88:
+        return False
+    elif distance(xvals,yvals,'INDEX_TIP') > 98:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') > 98:
+        return False
+    else:
+        return True
+def detect_f():
+    if distance(xvals,yvals,'PINKY_TIP') < 260:
+            return False
+    elif distance(xvals,yvals,'RING_TIP') < 260:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') < 260:
+        return False
+    elif distance(xvals,yvals,'INDEX_TIP') > 150:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') > 150:
+        return False
+    else:
+        return False
+def detect_g():
+    if distance(xvals,yvals,'INDEX_TIP') < 150:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') < 100:
+        return False
+    elif distance(xvals,yvals,'PINKY_TIP') > 80:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') > 90:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') > 95:
+        return False
+    else:
+        return True
+def detect_h():
+    if distance(xvals,yvals,'INDEX_TIP') < 320:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') < 320:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') > 180:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') > 180:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') > 170:
+        return False
+    else:
+        return True
+def detect_i():
+    #220 70
+    if distance(xvals,yvals,'PINKY_TIP') < 210:
+        return False
+    elif distance(xvals,yvals,'RING_TIP') > 75:
+        return False
+    elif distance(xvals,yvals,'INDEX_TIP') > 80:
+        return False
+    elif distance(xvals,yvals,'MIDDLE_TIP') > 90:
+        return False
+    elif distance(xvals,yvals,'THUMB_TIP') > 70:
+        return False
+    else:
+        return True
+
+global points
+points = ["WRIST", "THUMB_BASE", "THUMB_BOTTOM", "THUMB_MIDDLE", "THUMB_TIP", "INDEX_BASE", "INDEX_BOTTOM", "INDEX_MIDDLE", "INDEX_TIP","MIDDLE_BASE", "MIDDLE_BOTTOM", "MIDDLE_MIDDLE", "MIDDLE_TIP", "RING_BASE", "RING_BOTTOM", "RING_MIDDLE", "RING_TIP", "PINKY_BASE", "PINKY_BOTTOM", "PINKY_MIDDLE", "PINKY_TIP"]
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+def distance(xvals, yvals, point: str):
+    for i in range(21):
+        if points[i] == point:
+            id = i
+    for i in range(21):
+        if i == id:
+            wrist = xvals[0], yvals[0]
+            tip = xvals[id], yvals[id]
+            return math.dist(wrist,tip) 
+        else:
+            continue
+check = True
+while 1:
+    time.sleep(0.5)
+    xvals = []
+    yvals = []
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    zval = 0
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #print(points[id], cx, cy)
+                xvals.append(cx)
+                yvals.append(cy)
+                if id ==0:
+                    zval = lm.z*100000000
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    print(zval)
+    
+    if zval > 100 and check:
+        cv2.putText(img,"move back",(120,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    elif zval < 80 and check:
+        cv2.putText(img,"Move Closer",(120,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    else:
+        cv2.putText(img,"Hold",(120,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+        check = False
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    try:
+        print(f"d for pinky {distance(xvals,yvals,'PINKY_TIP')}")
+        print(f"d for ring {distance(xvals,yvals,'RING_TIP')}")
+        print(f"d for middle {distance(xvals,yvals,'MIDDLE_TIP')}")
+        print(f"d for index {distance(xvals,yvals,'INDEX_TIP')}")
+        print(f"d for thumb {distance(xvals,yvals,'THUMB_TIP')}")
+        if detect_a():
+            print("User is signing A!")
+        # if detect_b():
+        #     print("User is signing B")
+        # if detect_c():
+        #     print("User is signing C")
+        # if detect_d():
+        #     print("User is signing D")
+        # if detect_e():
+        #     print("User is signing E")
+        # if detect_f():
+        #     print("USer is signing F")
+        # if detect_g():
+        #     print("User is signing G")
+        # if detect_h():
+        #     print("User is signing H")
+        # if detect_i():
+        #     print("User is signing I")
+    except Exception as e:
+        print(e)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
